e114b2d5a13ede30205c6921ef6c68dd6419d9a8
jborelli95 <jborelli95@gmail.com>
2024-10-10 11:28:06 -0300

    add navbar, footer, hero and we are working in product list section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from './views/navegation/home-view/home-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeViewComponent
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './views/navegation/home-view/home-view.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { ProductSectionComponent } from './components/products/product-section/product-section.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    NavbarComponent,
+    HeroComponent,
+    ProductSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
